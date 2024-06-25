b3fe3cb7125db6decfc1ad104640d1883d418647
Efita Effiom <service.sangisang@gmail.com>
2024-06-25 11:54:46 +0100

    importedRepo

---
+++ config/passport-setup.js
@@ -0,0 +1,41 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+require('dotenv/config');
+const User = require('../models/user-model');
+
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+    User.findById(id).then((user) => {
+        done(null, user);
+    });
+});
+
+passport.use(
+    new GoogleStrategy({
+        clientID: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        callbackURL: '/auth/google/redirect'
+    }, (accessToken, refreshToken, profile, done) => {
+        // check if user already exists in our own db
+        User.findOne({ googleId: profile.id }).then((currentUser) => {
+            if (currentUser) {
+                // already have this user
+                console.log('user is: ', currentUser);
+                done(null, currentUser);
+            } else {
+                // if not, create user in our db
+                new User({
+                    googleId: profile.id,
+                    username: profile.displayName,
+                    thumbnail: profile.photos[0].value // updated to use profile.photos
+                }).save().then((newUser) => {
+                    console.log('created new user: ', newUser);
+                    done(null, newUser);
+                });
+            }
+        });
+    })
+);
